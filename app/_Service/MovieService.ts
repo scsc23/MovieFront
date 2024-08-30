e5312e6d0385033ee5eebb13a2cdabc7de6e20cb
scsc23 <xogmlm0507@gmail.com>
2024-08-30 17:26:54 +0900

    dd

--- app/_Service/MovieService.ts
+++ app/_Service/MovieService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://ec2-3-35-122-73.ap-northeast-2.compute.amazonaws.com:5000/api/movies";
+const API_BASE_URL = "http://MovieBack-env.eba-pmi92aap.ap-northeast-2.elasticbeanstalk.com:5000/api/movies";
 
 export const getNowPlaying = async () => {
   try {
