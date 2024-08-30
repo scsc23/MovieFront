14bd97572bd69373ea358d4a6e3aef6597ceb182
scsc23 <xogmlm0507@gmail.com>
2024-08-31 05:34:20 +0900

    ss

---
+++ .platform/hooks/postdeploy/01_start_server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# 기존의 next start 명령어 대신 standalone 서버 실행
+node /var/app/current/.next/standalone/server.js
--- .platform/hooks/predeploy/01_install_dependencies.sh
+++ .platform/hooks/predeploy/01_install_dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-npm cache clean --force
-npm ci
+# npm cache clean --force
+# npm ci
 chmod -R 755 node_modules
