01262c6722323120850156594e65678860df85ae
scsc23 <xogml0507@gmail.com>
2024-09-02 13:06:46 +0900

    ad

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,16 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import dynamic from "next/dynamic";
 import { AuthProvider } from "./(context)/AuthContext";
 import React from "react";
-import {ThemeProvider} from "@/(components)/DarkModToggle/ThemeContext";
-import Script from 'next/script'
+import { ThemeProvider } from "@/(components)/DarkModToggle/ThemeContext";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
-const Sidebar = dynamic(() => import('./(components)/Sidebar/Sidebar'), { ssr: false });
-
 export const metadata: Metadata = {
-  title: "MovieProject",
-  description: "Movie",
+  title: "Your Site Title",
+  description: "Your Site Description",
 };
 
 export default function RootLayout({
@@ -26,18 +23,27 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <head>
-       <meta name="monetag" content="08ca9a7e7521b15ee85b2d898ee9121d" />
       </head>
-
       <body>
-      <ThemeProvider>
+        <ThemeProvider>
           <AuthProvider>
-          <Sidebar />
-            {children}          
+            <ins
+              className="kakao_ad_area"
+              style={{ display: "none" }}
+              data-ad-unit="DAN-aq3kjg7Rp6i1bhxu"
+              data-ad-width="160"
+              data-ad-height="600"
+            ></ins>
+            {children}
             {modal}
-          <div id="modal-root"></div>
+            <Script
+              type="text/javascript"
+              src="//t1.daumcdn.net/kas/static/ba.min.js"
+              strategy="afterInteractive" // 클라이언트에서만 실행되도록 설정
+              async
+            ></Script>
           </AuthProvider>
-      </ThemeProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
